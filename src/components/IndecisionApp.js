7b8033fd98e6530720d46dbb327ddee07701ddf6
bushmb <bushmb@gmail.com>
2018-03-09 14:40:59 -0800

    added babel transform-class-properties

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,16 +6,9 @@ import Action from "./Action";
 import Header from "./Header";
 
 export default class IndesicionApp extends Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
+  state = {
+    options: []
+  };
 
   componentDidMount() {
     //checks for valid JSON data
@@ -36,7 +29,6 @@ export default class IndesicionApp extends Component {
     if (prevState.options.length !== this.state.options.length) {
       const json = JSON.stringify(this.state.options);
       localStorage.setItem("options", json);
-      console.log("saving data");
     }
   }
 
@@ -44,23 +36,23 @@ export default class IndesicionApp extends Component {
     console.log("componentWillUnmount");
   }
 
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   }
 
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState(prevState => ({
       options: prevState.options.filter(option => optionToRemove !== option)
     }));
   }
 
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
 
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
       return "Enter valid value to add item";
     } else if (this.state.options.indexOf(option) > -1) {
